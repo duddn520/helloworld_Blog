6b18c44f993e634c4d15ea2e2bd9b1823c9b62be
youngwoo <duddn520@naver.com>
2022-09-26 13:48:10 +0900

    Initial commit

---
+++ src/main/java/com/helloworldweb/helloworld_guestbook/HelloworldGuestbookApplication.java
@@ -0,0 +1,13 @@
+package com.helloworldweb.helloworld_guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldGuestbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldGuestbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworldweb/helloworld_guestbook/HelloworldGuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworldweb.helloworld_guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldGuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
